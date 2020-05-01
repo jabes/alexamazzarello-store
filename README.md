b1d5ed03dcc21cf7cfb35c7b409d6b545092abd9
Justin Bull <justin.bull@lightspeedhq.com>
2020-05-01 18:06:13 -0400

    Add scaffolding

---
+++ README.md
@@ -0,0 +1,3 @@
+Alexa Mazzarello Netlify Site
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/89c15605-0126-4436-a41e-25780018592c/deploy-status)](https://app.netlify.com/sites/alexamazzarello/deploys)
---
+++ nuxt.config.js
@@ -0,0 +1,79 @@
+import glob from 'glob'
+import path from 'path'
+
+const dynamicRoutes = getDynamicPaths({
+    '/blog': 'blog/*.json',
+    '/page': 'page/*.json',
+});
+
+export default {
+    mode: 'universal',
+
+    head: {
+        title: "Alexa Mazzarello",
+        meta: [
+            {
+                charset: 'utf-8'
+            },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: "Static content generator for ecom store front."
+            }
+        ],
+        link: [
+            {
+                rel: 'icon',
+                type: 'image/x-icon',
+                href: '/favicon.ico'
+            }
+        ]
+    },
+
+    loading: {
+        color: '#fff'
+    },
+
+    css: [],
+    plugins: [],
+
+    modules: [
+        '@nuxtjs/markdownit',
+        '@nuxtjs/axios',
+        '@nuxtjs/pwa',
+    ],
+
+    markdownit: {
+        injected: true,
+        preset: 'default',
+        breaks: true,
+        html: true,
+    },
+
+    axios: {
+        // See https://github.com/nuxt-community/axios-module#options
+    },
+
+    build: {
+        extend(config, ctx) {},
+    },
+
+    generate: {
+        routes: dynamicRoutes
+    }
+}
+
+function getDynamicPaths(urlFilepathTable) {
+    return [].concat(
+        ...Object.keys(urlFilepathTable).map(url => {
+            let filepathGlob = urlFilepathTable[url];
+            return glob
+                .sync(filepathGlob, {cwd: 'content'})
+                .map(filepath => `${url}/${path.basename(filepath, '.json')}`);
+        })
+    );
+}
